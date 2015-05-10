8f17a710ed203c662ef77c9c09feeb110e2fad26
rasheed.amir <rasheed@aurorasolutions.io>
2015-05-10 10:50:51 +0200

    updated the message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello PyCon Sweden 2015!"
+    return "Welcome To PyCon Sweden 2015!"
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
